ec13b690d04bec0d26fc666faf153776a2cb1b2f
Tanmeet Singh Chhabra <tanmeet.chhabra.0@gmail.com>
2024-08-14 18:20:16 +0530

    finished creating the temperature conversion app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import "temperature_conversion.dart";
+
+// Todos
+// 1. add enum for temp types
+// 2. add animation when numbers change
 
 void main() {
   runApp(const MyApp());
@@ -6,120 +12,222 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var colour_scheme = ColorScheme.dark();
+    var app_theme = ThemeData(useMaterial3: true, colorScheme: colour_scheme);
+    app_theme = app_theme.copyWith(
+        textTheme: app_theme.textTheme.apply(
+            bodyColor: colour_scheme.primary,
+            displayColor: colour_scheme.primary));
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Temperature Conversion",
+      home: MyHomePage(),
+      theme: app_theme,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() {
+    return _MyHomePageState();
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double temperature_measurement_in_kelvin = 0;
+  double temperature_measurement_in_celsius = 0;
+  double temperature_measurement_in_fahrenheit = 0;
+ 
 
-  void _incrementCounter() {
+  void on_kelvin_input(String kelvin_input) {
+    double kelvin_measurement = double.parse(kelvin_input);
+    double celsius_measurement = convert_kelvin_to_celsius(kelvin_measurement);
+    double fahrenheit_measurement =
+        convert_celsius_to_fahrenheit(celsius_measurement);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      temperature_measurement_in_kelvin = kelvin_measurement;
+      temperature_measurement_in_celsius = celsius_measurement;
+      temperature_measurement_in_fahrenheit = fahrenheit_measurement;
+    });
+  }
+  void on_celsius_input(String celsius_input) {
+    double celsius_measurement = double.parse(celsius_input);
+    double kelvin_measurement = convert_celsius_to_kelvin(celsius_measurement);
+    double fahrenheit_measurement =
+        convert_celsius_to_fahrenheit(celsius_measurement);
+    setState(() {
+      temperature_measurement_in_kelvin = kelvin_measurement;
+      temperature_measurement_in_celsius = celsius_measurement;
+      temperature_measurement_in_fahrenheit = fahrenheit_measurement;
+    });
+  }
+
+  void on_fahrenheit_input(String fahrenheit_input) {
+    double fahrenheit_measurement = double.parse(fahrenheit_input);
+    double celsius_measurement =
+        convert_fahrenheit_to_celsius(fahrenheit_measurement);
+    double kelvin_measurement = convert_celsius_to_kelvin(celsius_measurement);
+    setState(() {
+      temperature_measurement_in_kelvin = kelvin_measurement;
+      temperature_measurement_in_celsius = celsius_measurement;
+      temperature_measurement_in_fahrenheit = fahrenheit_measurement;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final context_theme = Theme.of(context);
+    final title_text_style = context_theme.textTheme.displayMedium;
+    final table_heading_text_style = context_theme.textTheme.displaySmall;
+    final app_bar_bg_color = context_theme.colorScheme.onPrimary;
+    const double space_between_rows = 50;
+    const blank_separating_row = TableRow(
+      children: [
+        SizedBox(
+          height: space_between_rows,
+        ),
+        SizedBox(
+          height: space_between_rows,
+        ),
+        SizedBox(
+          height: space_between_rows,
+        ),
+      ],
+    );
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        backgroundColor: app_bar_bg_color,
+        toolbarHeight: 80,
+        title: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Temperature Conversion",
+              style: title_text_style,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 150),
+          child: Table(
+            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+            children: [
+              TableRow(
+                children: [
+                  Text(
+                    "Temperature Scale",
+                    textAlign: TextAlign.center,
+                    style: table_heading_text_style,
+                  ),
+                  Text(
+                    "Input",
+                    textAlign: TextAlign.center,
+                    style: table_heading_text_style,
+                  ),
+                  Text(
+                    "Output",
+                    textAlign: TextAlign.center,
+                    style: table_heading_text_style,
+                  ),
+                ],
+              ),
+              blank_separating_row,
+              TemperatureTableRow(
+                  temperature_scale: "Kelvin",
+                  temperature_measurement:
+                      temperature_measurement_in_kelvin.toStringAsFixed(2),
+                  on_submit_callback: on_kelvin_input,
+                  theme: context_theme),
+              blank_separating_row,
+              TemperatureTableRow(
+                  temperature_scale: "Celsius",
+                  temperature_measurement:
+                      temperature_measurement_in_celsius.toStringAsFixed(2),
+                  on_submit_callback: on_celsius_input,
+                  theme: context_theme),
+              blank_separating_row,
+              TemperatureTableRow(
+                  temperature_scale: "Fahrenheit",
+                  temperature_measurement:
+                      temperature_measurement_in_fahrenheit.toStringAsFixed(2),
+                  on_submit_callback: on_fahrenheit_input,
+                  theme: context_theme),
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+TableRow TemperatureTableRow(
+    {required String temperature_scale,
+    required String temperature_measurement,
+    required void Function(String) on_submit_callback,
+    required ThemeData theme}) {
+  final text_style = theme.textTheme.displaySmall;
+  final TextEditingController text_controller = TextEditingController();
+  var degree_symbol = "°";
+  if (temperature_scale == "Kelvin") {
+    degree_symbol = "";
+  }
+  return TableRow(
+    children: [
+      Text(
+        textAlign: TextAlign.center,
+        temperature_scale,
+        style: text_style,
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: TextFormField(
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: theme.colorScheme.primary)),
+            focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 3, color: theme.colorScheme.primary)),
+            contentPadding: EdgeInsets.all(10),
+            fillColor: theme.colorScheme.onPrimary,
+            filled: true,
+          ),
+          textAlign: TextAlign.center,
+          controller: text_controller,
+          keyboardType:
+              TextInputType.numberWithOptions(decimal: true, signed: true),
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(
+                RegExp(r"(^-?)([0-9]*)(\.?)([0-9]*)"))
+          ],
+          // regex explanation - 4 groups represented by ()
+          // first group : at the beginning of the string, '-' symbol 0 or 1 times
+          // second group : 0-9 digits 0 or more times
+          // third group : '.' symbol 0 or 1 times, '\' used to escape '.'
+          // fourth group : 0-9 digits 0 or more times
+          style: text_style,
+          onFieldSubmitted: (value) {
+            if (value != "") {
+              on_submit_callback(value);
+            }
+            text_controller.clear();
+          },
+        ),
+      ),
+      DecoratedBox(
+        decoration: BoxDecoration(),
+        child: Text(
+          textAlign: TextAlign.center,
+          "$temperature_measurement $degree_symbol${temperature_scale[0]}",
+          style: text_style,
+        ),
+      ),
+    ],
+  );
 }
